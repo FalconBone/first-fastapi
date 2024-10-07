ea89ff2e58c0936ee155ed58636732959e4489fb
Happy <happycatvasiliy9786@gmail.com>
2024-10-07 15:53:57 +0300

    first commit

---
+++ schemas.py
@@ -0,0 +1,14 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class STaskAdd(BaseModel):
+    name: str
+    description: Optional[str] = None
+
+class STask(STaskAdd):
+    id: int
+
+class STaskId(BaseModel) :
+    id: int
+    ok: bool = True
